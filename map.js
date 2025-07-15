b4dc22511fccd2bc67627fb6b21dec2be0b432e0
Derek Song <dereksong28@gmail.com>
2025-07-15 01:26:12 -0400

    Forked gis files from FlightPath Downsview and a simplified interface based on the React interfaces

--- map.js
+++ map.js
@@ -1,4 +1,5 @@
-const map = L.map('map').setView([43.8094086, -79.2696282], 13);
+
+const map = L.map('map').setView([43.7416, -79.4804406], 16);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://carto.com/attributions">CartoDB</a>',
@@ -10,13 +11,14 @@ const files = [
   "Hummingbird.geojson",
   "LakeLookout.geojson",
   "NorthFarm.geojson",
-  "NorthHill.geojson",
+  "NorthPlaza.geojson",
   "Orchard.geojson",
-  "OtherPond.geojson",
+  "LowerPond.geojson",
   "Playground.geojson",
   "SesquicentennialMonument.geojson",
-  "SouthHill.geojson",
+  "MiniMound.geojson",
   "SwanLake.geojson",
+  "Mound.geojson",
   "UrbanFarm.geojson"];
 
 const polygons = {
@@ -34,14 +36,34 @@ Promise.all(
 
     L.geoJSON(data, {
       onEachFeature: function (feature, layer) {
-        layer.on('click', function (e) {
-          const popupContent = `<strong>${feature.properties.name}</strong>`;
-          layer.bindPopup(popupContent).openPopup(e.latlng);
-        });
+        const { id, heroImage, description } = feature.properties;
+        const popupContent = `
+        <div class="p-2 text-sm max-w-[600px]">
+          <div class="w-[350px] h-[150px] overflow-hidden rounded-t-lg">
+            <img src="${heroImage}" alt="${id}"
+                class="w-full h-full object-cover" />
+          </div>
+          <div class="w-[350px] bg-[#538a53] text-white p-2 rounded-b-lg">
+            <h2>${id}</h2>
+          </div>
+          <div class="max-w-[350px]">
+            <p>${description}</p>
+          </div>
+        </div>
+        `;
+        layer.bindPopup(popupContent, { maxWidth: 600 });
+        layer.bindTooltip(id, {
+          permanent: true,
+          direction: "center",
+          className: "polygon-label",
+          offset: [0, 0]
+        }).openTooltip();
+
+        
       },
       style: {
         color: "#3388ff",
-        fillOpacity: 0.5
+        fillOpacity: 0.2
       }
     }).addTo(map);
   });
